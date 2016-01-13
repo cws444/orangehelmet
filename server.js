b93858e1796f9cd882383873c390d368fcc962d1
cws444 <cws444@hotmail.com>
2016-01-13 16:17:53 +0800

    Updates to Project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const data = require('./quiz1.json')
+
+app.get('/quiz1', function (req, res) {
+  res.send(data)
+})
+
+app.use(express.static('public'))
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Quiz app listening at http://%s:%s', host, port);
+})
